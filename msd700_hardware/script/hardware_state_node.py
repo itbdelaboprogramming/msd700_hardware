b4c054a47234fdf877e4fecb2fbb5d0be633eb17
goldwinsonick <gswt.code@gmail.com>
2024-12-22 19:51:18 +0700

    feat: add msd700_hardware (odom, serial, lidar)

---
+++ msd700_hardware/script/hardware_state_node.py
@@ -0,0 +1,77 @@
+# --- hardware_state_parser.py ---
+# Parse the HardwareState msg and publish the datas
+
+# Libraries
+import rospy
+import tf
+import numpy as np
+
+# Msgs
+from msd700_msgs.msg import HardwareState
+from sensor_msgs.msg import Imu, MagneticField
+from geometry_msgs.msg import Quaternion, Vector3
+from std_msgs.msg import Float32MultiArray
+
+class HardwareStateParser:
+    def __init__(self) -> None:
+        rospy.init_node('hardware_state_parser')
+
+        # Variables
+        self.right_motor_pulse_delta = 0.0
+        self.left_motor_pulse_delta = 0.0
+        self.roll, self.pitch, self.yaw = 0.0, 0.0, 0.0
+        self.acc_x, self.acc_y, self.acc_z = 0.0, 0.0, 0.0
+        self.gyro_x, self.gyro_y, self.gyro_z = 0.0, 0.0, 0.0
+        self.mag_x, self.mag_y, self.mag_z = 0.0, 0.0, 0.0
+
+        # Publisher
+        self.imu_raw_pub        = rospy.Publisher('/imu/data_raw'           , Imu               , queue_size=1)
+        self.mag_pub            = rospy.Publisher('/imu/mag'                , MagneticField     , queue_size=1)
+        self.motor_pulse_pub    = rospy.Publisher('/hardware/motor_pulse'   , Float32MultiArray , queue_size=1)
+
+    def hardware_state_callback(self, msg: HardwareState) -> None:
+        # Update raw values
+        self.right_motor_pulse_delta = msg.right_motor_pulse_delta
+        self.left_motor_pulse_delta = msg.left_motor_pulse_delta
+
+        self.roll = np.radians(msg.roll)
+        self.pitch = np.radians(msg.pitch)
+
+        self.acc_x = msg.acc_x
+        self.acc_y = msg.acc_y
+        self.acc_z = msg.acc_z + self.az_offset
+
+        self.gyr_x = np.radians(msg.gyr_y)
+        self.gyr_y = np.radians(msg.gyr_x)
+        self.gyr_z = np.radians(msg.gyr_z)
+
+        self.mag_x = msg.mag_x / 1e6
+        self.mag_y = msg.mag_y / 1e6
+        self.mag_z = msg.mag_z / 1e6
+
+    def publish_imu_raw(self, roll: float, pitch: float, yaw: float
+                        , acc_x: float, acc_y: float, acc_z: float
+                        , gyr_x: float, gyr_y: float, gyr_z: float) -> None:
+        imu_raw_msg = Imu()
+        imu_raw_msg.header.stamp = rospy.Time.now()
+        imu_raw_msg.header.frame_id = 'imu_raw'
+        imu_raw_msg.orientation = Quaternion(*tf.transformations.quaternion_from_euler(roll, pitch, yaw))
+        imu_raw_msg.angular_velocity = Vector3(gyr_x, gyr_y, gyr_z)
+        imu_raw_msg.linear_acceleration = Vector3(acc_x, acc_y, acc_z)
+        self.imu_raw_pub.publish(imu_raw_msg)
+    
+    def publish_mag(self, mag_x: float, mag_y: float, mag_z: float) -> None:
+        mag_msg = MagneticField()
+        mag_msg.header.stamp = rospy.Time.now()
+        mag_msg.header.frame_id = 'magnetometer'
+        mag_msg.magnetic_field = Vector3(mag_x, mag_y, mag_z)
+        self.mag_pub.publish(mag_msg)
+    
+    def publish_motor_pulse(self, right_motor_pulse_delta: float, left_motor_pulse_delta: float) -> None:
+        motor_pulse_msg = Float32MultiArray()
+        motor_pulse_msg.data = [right_motor_pulse_delta, left_motor_pulse_delta]
+        self.motor_pulse_pub.publish(motor_pulse_msg)
+
+if __name__ == '__main__':
+    rospy.init_node("hardware_state_parser", anonymous=True)
+    HardwareStateParser()
---
+++ msd700_hardware/script/odom_node.py
@@ -0,0 +1,100 @@
+# --- odom_node.py ---
+# Publishes odometry data (topic and tf) from motor pulses and IMU data
+
+import os, sys
+import rospy
+import tf
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped, Point, Quaternion, Vector3
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Float32MultiArray
+import numpy as np
+
+class OdomNode:
+    def __init__(self):
+        # Publisher
+        self.odom_pub   = rospy.Publisher('/odom', Odometry, queue_size=1)
+        self.pose_pub   = rospy.Publisher('/pose', PoseStamped, queue_size=1)
+
+        # TF Broadcaster
+        self.odom_broadcaster   = tf.TransformBroadcaster()
+
+        # Subscriber
+        self.imu_sub            = rospy.Subscribe('/imu/data', Imu, self.imu_callback)
+        self.motor_pulse_sub    = rospy.Subscribe('/hardware/motor_pulse', Float32MultiArray, self.motor_pulse_callback)
+
+        # Parameters
+        self.compute_period     = rospy.get_param('compute_period', 30)     # ms
+        self.encoder_ppr        = rospy.get_param('encoder_ppr', 1024)      # ppr
+        self.wheel_distance     = rospy.get_param('wheel_distance', 0.23)   # meter
+        self.wheel_distance     = rospy.get_param('wheel_radius', 0.23)     # meter
+        self.use_imu            = rospy.get_param('use_imu', True)
+        self.debug              = rospy.get_param('debug', False)
+
+        # Variables
+        # Odometry data
+        self.linear_velocity = 0.0
+        self.angular_velocity = 0.0
+        # Motor Pulses
+        self.right_motor_pulse_delta = 0.0
+        self.left_motor_pulse_delta = 0.0
+        # IMU data
+        self.rpy = [0.0, 0.0, 0.0]
+        self.gyro = [0.0, 0.0, 0.0]
+        # Timing
+        self.last_time = None
+
+        rospy.Timer(rospy.Duration(self.compute_period/1000), self.update_odom)
+
+    # --- Sensor Callbacks ---
+    def imu_callback(self, msg: Imu) -> None:
+        # -- IMU DATA --
+        # Get roll pitch yaw
+        ort_q = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion(ort_q)
+        self.rpy = [roll, pitch, yaw]
+
+        # Get gyro
+        self.gyro = [msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z]
+
+    def motor_pulse_callback(self, msg: Float32MultiArray):
+        # Get motor pulses
+        self.right_motor_pulse_delta = msg.data[0]
+        self.left_motor_pulse_delta = msg.data[1]
+    
+    def update_odom(self, event) -> None:
+        # Reference:
+        # - https://medium.com/@nahmed3536/wheel-odometry-model-for-differential-drive-robotics-91b85a012299
+        
+        current_time = rospy.Time.now()
+        dt = (current_time - self.last_time).to_sec()
+        self.last_time = current_time
+
+        # Convert the motor pulses to distance (meter)
+        pulses_to_distance = (2 * np.pi * self.wheel_radius) / self.encoder_ppr
+        d_right = self.right_motor_pulse_delta * pulses_to_distance
+        d_left = self.left_motor_pulse_delta * pulses_to_distance
+
+        # Compute the delta dist (d) and theta (dtheta)
+        d = (d_right + d_left) / 2.0
+        dtheta = (d_right - d_left) / self.wheel_distance
+
+        # Update pose
+        self.x += d * np.cos(self.theta)
+        self.y += d * np.sin(self.theta)
+        self.theta += dtheta
+
+        if self.use_imu:
+            self.theta = self.rpy[2]
+
+        # Debug
+        if self.debug:
+            rospy.loginfo(f"---")
+            rospy.loginfo(f'dt: {dt}s, d: {d}m, dtheta: {dtheta}m')
+            rospy.loginfo(f'x: {self.x}, y: {self.y}, theta: {self.theta}')
+            rospy.loginfo(f"---")
+    
+if __name__ == '__main__':
+    rospy.init_node('odom_node')
+    odom_node = OdomNode()
+    rospy.spin()
