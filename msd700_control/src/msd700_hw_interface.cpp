0a96cf157ba60c1745026ec9d7975e771b807506
goldwinsonick <gswt.code@gmail.com>
2025-02-15 16:29:28 +0700

    feat: fixed msd700_control, msd700_description, added gazebo control, etc

--- msd700_control/src/msd700_hw_interface.cpp
+++ msd700_control/src/msd700_hw_interface.cpp
@@ -10,38 +10,46 @@ class MSD700HWInterface : public hardware_interface::RobotHW {
 public:
     MSD700HWInterface(ros::NodeHandle nh) : nh_(nh) {
         // Register joint state interface
-        // (Read the current state of the robot)
-        hardware_interface::JointStateHandle left_wheel_handle("left_wheel_joint", &pos_[0], &vel_[0], &eff_[0]);
-        jnt_state_interface.registerHandle(left_wheel_handle);
-        hardware_interface::JointStateHandle right_wheel_handle("right_wheel_joint", &pos_[1], &vel_[1], &eff_[1]);
-        jnt_state_interface.registerHandle(right_wheel_handle);
+        // This handles the current state of the robot.
+        // We can update these values in the read() function.
+        // In simulation, these values are updated by the gazebo plugin
+        hardware_interface::JointStateHandle fl_handle("fl_wheel_joint", &pos_[0], &vel_[0], &eff_[0]);
+        hardware_interface::JointStateHandle bl_handle("bl_wheel_joint", &pos_[1], &vel_[1], &eff_[1]);
+        hardware_interface::JointStateHandle fr_handle("fr_wheel_joint", &pos_[2], &vel_[2], &eff_[2]);
+        hardware_interface::JointStateHandle br_handle("br_wheel_joint", &pos_[3], &vel_[3], &eff_[3]);
+
+        jnt_state_interface.registerHandle(fl_handle);
+        jnt_state_interface.registerHandle(bl_handle);
+        jnt_state_interface.registerHandle(fr_handle);
+        jnt_state_interface.registerHandle(br_handle);
+
         registerInterface(&jnt_state_interface);
 
         // Register joint velocity interface
-        // (Write the velocity command to the robot)
-        hardware_interface::JointHandle left_wheel_vel_handle(jnt_state_interface.getHandle("left_wheel_joint"), &cmd_[0]);
-        jnt_vel_interface.registerHandle(left_wheel_vel_handle);
-        hardware_interface::JointHandle right_wheel_vel_handle(jnt_state_interface.getHandle("right_wheel_joint"), &cmd_[1]);
-        jnt_vel_interface.registerHandle(right_wheel_vel_handle);
+        // This is where controllers write their commands.
+        // We can convert these commands to actual motor commands in the write() function.
+
+        hardware_interface::JointHandle fl_vel_handle(jnt_state_interface.getHandle("fl_wheel_joint"), &cmd_[0]);
+        hardware_interface::JointHandle bl_vel_handle(jnt_state_interface.getHandle("bl_wheel_joint"), &cmd_[0]);
+        hardware_interface::JointHandle fr_vel_handle(jnt_state_interface.getHandle("fr_wheel_joint"), &cmd_[1]);
+        hardware_interface::JointHandle br_vel_handle(jnt_state_interface.getHandle("br_wheel_joint"), &cmd_[1]);
+
+        jnt_vel_interface.registerHandle(fl_vel_handle);
+        jnt_vel_interface.registerHandle(bl_vel_handle);
+        jnt_vel_interface.registerHandle(fr_vel_handle);
+        jnt_vel_interface.registerHandle(br_vel_handle);
+
         registerInterface(&jnt_vel_interface);
 
-        // Parameters
-        nh.param("wheel_radius"     , wheel_radius, 0.1);
-        nh.param("wheel_separation" , wheel_separation, 0.2);
-        nh.param("motor_ppr"        , motor_ppr, 2400.0);
-        nh.param("enable_odom"      , enable_odom, false);
-
         // Initial variables
-        pos_[0] = pos_[1] = 0.0;
-        vel_[0] = vel_[1] = 0.0;
-        eff_[0] = eff_[1] = 0.0;
+        for (int i = 0; i < 4; i++) {
+            pos_[i] = 0.0;
+            vel_[i] = 0.0;
+            eff_[i] = 0.0;
+        }
         cmd_[0] = cmd_[1] = 0.0;
 
-        ros::Publisher hardware_command_pub = nh_.advertise<msd700_msgs::HardwareCommand>("/hardware_command", 1);
-
-        if(enable_odom){
-            ros::Subscriber motor_pulse_sub = nh_.subscribe("/hardware/motor_pulse", 10, &MSD700HWInterface::motor_pulse_callback, this);
-        }
+        hardware_command_pub_ = nh_.advertise<msd700_msgs::HardwareCommand>("/hardware_command", 1);
 
     }
 
@@ -50,88 +58,71 @@ public:
     }
 
     void write(){
+        // --- Publish HardwareCommand to Firmware based on cmd_ ---
+        // The cmd_ controlled by controller_manager (diff_drive_controller)
+
         // rpm = (rad/s) * (60 / (2*pi))
         double left_rpm = cmd_[0] * (60.0 / (2.0 * M_PI));
         double right_rpm = cmd_[1] * (60.0 / (2.0 * M_PI));
         
+        // Publish the msg
         msd700_msgs::HardwareCommand cmd_msg;
         cmd_msg.movement_command    = 0;            // Not used
         cmd_msg.cam_angle_command   = 0;            // Not used
         cmd_msg.right_motor_speed   = right_rpm;
         cmd_msg.left_motor_speed    = left_rpm;
 
-        // Publish the message to your motor controller topic
-        hardware_command_pub.publish(cmd_msg);
+        hardware_command_pub_.publish(cmd_msg);
     }
 
 private:
-
-    void motor_pulse_callback(const std_msgs::Float32MultiArray::ConstPtr& msg) {
-        if(!enable_odom){
-            return;
-        }
-
-        double now = ros::Time::now().toSec();
-        double delta_time = now - last_time;
-
-        // read pulse
-        double delta_left_pulse = msg->data[0];
-        double delta_right_pulse = msg->data[1];
-
-        // rpm = (pulse) * (60 / ppr)
-        double delta_left_rpm = delta_left_pulse * (60.0 / motor_ppr);
-        double delta_right_rpm = delta_right_pulse * (60.0 / motor_ppr);
-
-        // m = (rpm) * (2*pi / 60)
-        double delta_left_m = delta_left_rpm * (2.0 * M_PI / 60.0);
-        double delta_right_m = delta_right_rpm * (2.0 * M_PI / 60.0);
-
-        // update time
-        last_time = now;
-
-        return;
-    }
+    // ROS NodeHandle
+    ros::NodeHandle nh_;
+    ros::Publisher hardware_command_pub_;
+    ros::Subscriber motor_pulse_sub;
 
     // Parameters
     double wheel_radius;
     double wheel_separation;
     double motor_ppr;
-    bool enable_odom;
 
+    // Interfaces
     hardware_interface::JointStateInterface jnt_state_interface;
     hardware_interface::VelocityJointInterface jnt_vel_interface;
     
     // Variables
-    double cmd_[2];
-    double pos_[2];
-    double vel_[2];
-    double eff_[2];
-
-    double last_time;
-
-    ros::NodeHandle nh_;
-    ros::Publisher hardware_command_pub;
-    ros::Subscriber motor_pulse_sub;
+    double pos_[4];    // positions for [fl, bl, fr, br]
+    double vel_[4];    // velocities for [fl, bl, fr, br]
+    double eff_[4];    // efforts for [fl, bl, fr, br]
+    double cmd_[2];    // commands for [left, right]
 
 };
 
 int main(int argc, char **argv) {
+    // https://docs.ros.org/en/noetic/api/hardware_interface/html/c++/index.html
+    // Initialize the node
     ros::init(argc, argv, "msd700_hw_interface");
     ros::NodeHandle nh;
 
-    MSD700HWInterface msd700_hw_interface(nh);
-    controller_manager::ControllerManager cm(&msd700_hw_interface, nh);
+    // Create the hardware interface and controller manager
+    MSD700HWInterface hw(nh);
+    controller_manager::ControllerManager cm(&hw, nh);
 
-    ros::Rate loop_rate(10);
+    // Set an update frequency (e.g., 50 Hz)
+    ros::Rate loop_rate(50);
     ros::Time last_time = ros::Time::now();
-    while (ros::ok()) {
+
+    while (ros::ok())
+    {
         ros::Time current_time = ros::Time::now();
-        ros::Duration period = current_time - last_time;
-
-        cm.update(current_time, period);
-        msd700_hw_interface.write();
-
+        ros::Duration elapsed_time = current_time - last_time;
         last_time = current_time;
+
+        hw.read();
+        cm.update(current_time, elapsed_time);
+        hw.write();
+
+        ros::spinOnce();
         loop_rate.sleep();
     }
 
